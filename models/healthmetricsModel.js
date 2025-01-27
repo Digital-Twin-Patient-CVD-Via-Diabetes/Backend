0ca83d51ded942b987efe3d7e9ae383ed9b21931
MostafaDarwish93 <MostafaDarwish9392@gmail.com>
2025-01-27 13:33:56 +0200

    feat: :card_file_box: init healthmetrics

---
+++ models/healthmetricsModel.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const metricsSchema = new mongoose.Schema({
+    patientId: {
+        type: String,
+        required: true,
+        ref: "Patient"
+    },
+    metricDate:{
+        type: Date,
+        
+    },
+    createDate:{
+        type: Date,
+    },
+    updatedDate:{
+        type: Date,
+    },
+    bloodPressure:{
+        type: Number,
+    },
+    bmi:{
+        type: Number,
+    },
+    glucose:{
+        type: Number,
+    },
+    cholesterolTotal:{
+        type: Number,
+    },
+    cholesterolHDL:{
+        type: Number,
+    },
+    cholesterolLDL:{
+        type: Number,
+    },
+    albuminCreatineUrine:{
+        type: Number,
+    },
+    troponinMedian:{
+        type: Number,
+    },
+    hemoglobinA1c:{
+        type: Number,
+    },
+    creatineKinaseCK:{
+        type: Number,
+    },
+    creatineKinaseMB:{
+        type: Number,
+    },
+    medianTriglycerides:{
+        type: Number,
+    },
+    medianNtprobnp:{
+        type: Number,
+    },
+    medianT3Value:{
+        type: Number,
+    },
+    thyroxineFreeLevel:{
+        type: Number,
+    }
+})
+
+const healthMetrics = mongoose.models.healthMetrics || mongoose.model("healthMetrics", metricsSchema);
