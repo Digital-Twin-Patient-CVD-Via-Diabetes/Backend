287f68c29d86a1aa7a8ccea67082acb7c960a3fe
Mohelghitany <elgetany1@gmail.com>
2025-03-21 18:28:23 +0200

    xx

--- controllers/medicienController.js
+++ controllers/medicienController.js
@@ -1,4 +1,6 @@
+import { get } from "mongoose";
 import mediciens from "../models/mediciens.model.js";
+import patients from "../models/patients.model.js";
 
 // Create and Save a new Medicien
 const createMedicien = async (req, res) => {
@@ -36,6 +38,37 @@ const getAllMediciens = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 }
+const getMedicien = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const patient = await patients.findById(id);
+        if (!patient) return res.status(404).json({ message: "Patient not found" });
+
+        const specialization = patient.disease;
+        if (!specialization || specialization === "none") {
+            return res.status(406).json({ message: "Specialization not found" });
+        }
+
+     const influenceFilter = { influence: "positive" };
+
+        if (specialization === "both") {
+            const [medicine1, medicine2] = await Promise.all([
+                mediciens.find({ specialization: "diabetes", ...influenceFilter }),
+                mediciens.find({ specialization: "cvd", ...influenceFilter })
+            ]);
+            medicien = [...medicine1, ...medicine2];
+           
+        } else {
+            medicien = await mediciens.find({ specialization });
+        }
+
+        return res.status(200).json({ medicien });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
 
 // Find a single medicien with an id
 // const getMedicien = async (req, res) => {
@@ -75,4 +108,4 @@ const deleteMedicien = async (req, res) => {
 }
 
 
-export { createMedicien, getAllMediciens, deleteMedicien };
+export { createMedicien, getMedicien ,getAllMediciens, deleteMedicien };
--- models/mediciens.model.js
+++ models/mediciens.model.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const mediciensSchema = new mongoose.Schema({
-    name: { type: String, required: true },                  
+    Medication: { type: String, required: true },
     specialization: { type: String, required: true },
-    company: { type: String, required: true },
+    Influence: { type: String, required: true },
 }, { timestamps: true });
 
 const mediciens = mongoose.models.mediciens || mongoose.model('mediciens', mediciensSchema);
